43822c53f48d7a9695685e92ef43061abdc1f31e
IlijaP81 <niilp1999no@gmail.com>
2024-07-26 13:11:16 +0300

    Добавьте файлы проекта.

---
+++ 1DimArraySorting/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+        int max, i = 0, j = arr.Length - 1;
+        bool arrayIsSorted = false;
+        
+        do
+        {
+            if (j == 0) arrayIsSorted = true;
+            if (i >= 0 & i < arr.Length - 1)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    max = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = max;
+                    arrayIsSorted = false;
+                }
+                else j--; // счетчик обходов массива
+            }
+            else
+            {
+                i = -1;
+                j = arr.Length - 1;
+            }
+            i++;
+        } 
+        while (!arrayIsSorted);
+
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
